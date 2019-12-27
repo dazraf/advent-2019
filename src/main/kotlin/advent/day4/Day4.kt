3008d920181c94e0fc3164b9374ae4fc73246f19
Fuzz <fuzz@dazraf.io>
2019-12-27 03:34:17 +0000

    refactoring day 4

--- src/main/kotlin/advent/day4/Day4.kt
+++ src/main/kotlin/advent/day4/Day4.kt
@@ -3,16 +3,14 @@ package advent.day4
 fun main() {
   val (start, end) = readLine()?.split('-')?.map { it.toInt() }
     ?: error("failed to read numbers")
+  val allCriterion = listOf(::atLeastOneIsolatedDouble, ::neverDecrements)
   println((start..end).count { matches(it.toString(), allCriterion) })
 }
 
-val allCriterion = listOf(::twoAdjacentAreEqual, ::neverDecrements)
-
 typealias Criterion = (String) -> Boolean
-
 fun matches(password: String, criteria: List<Criterion>) = criteria.all { it(password) }
 
-fun twoAdjacentAreEqual(password: String) =
+fun atLeastOneIsolatedDouble(password: String) =
   password.groupDuplicates().any { it.length == 2 }
 
 fun String.groupDuplicates(): List<String> {
