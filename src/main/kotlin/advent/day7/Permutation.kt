575360b6771ba432b636747037c48615c22af524
Fuzz <fuzz@dazraf.io>
2019-12-29 01:15:03 +0000

    day7 part 1

---
+++ src/main/kotlin/advent/day7/Permutation.kt
@@ -0,0 +1,22 @@
+package advent.day7
+
+fun <T> permutation(items: List<T>): Sequence<List<T>> {
+  return sequence {
+    when {
+      items.size <= 1 -> yield(items)
+      else -> {
+        items.forEachIndexed { pivot, item ->
+          // for each item
+          val remaining =
+            items.filterIndexed { idx, _ -> idx != pivot } // remove that item
+          permutation(remaining) // permute the remaining items
+            .forEach { remainingPermutation ->
+              // and for each permutation ...
+              // prepend the removed item, yielding another result
+              yield(listOf(item) + remainingPermutation)
+            }
+        }
+      }
+    }
+  }
+}
